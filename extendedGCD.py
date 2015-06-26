099881bc3aff5045b0d0ae17f4fbf242538cb033
Anwesh Mohanty <anwesh063@gmail.com>
2015-06-26 11:38:38 +0530

    Create extendedGCD.py

---
+++ extendedGCD.py
@@ -0,0 +1,8 @@
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return gcd, x, y
