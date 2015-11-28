b8981279cda5f30f04a8bc86a4a885cb9c918211
Anwesh Mohanty <anwesh063@gmail.com>
2015-11-28 18:30:06 +0530

    Create Trie.py

---
+++ Trie.py
@@ -0,0 +1,45 @@
+class Trie(object):
+    def __init__(self):
+        self.children = {}
+        self.flag = False # Flag to represent that a word ends at this node
+
+    def add(self, char):
+        self.children[char] = Trie()
+
+    def insert(self, word):
+        node = self
+        for char in word:
+            if char not in node.children:
+                node.add(char)
+            node = node.children[char]
+        node.flag = True
+
+    def contains(self, word):
+        node = self
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.flag
+
+    def all_suffixes(self, prefix):
+        results = set()
+        if self.flag:
+            results.add(prefix)
+        if not self.children: return results
+        return reduce(lambda a, b: a | b, [node.all_suffixes(prefix + char) for (char, node) in self.children.items()]) | results
+
+    def autocomplete(self, prefix):
+        node = self
+        for char in prefix:
+            if char not in node.children:
+                return set()
+            node = node.children[char]
+        return list(node.all_suffixes(prefix))
+
+x = Trie()
+x.insert('app')
+x.insert('apple')
+x.insert('apllo')
+for i in x.autocomplete('ap'):
+    print i
