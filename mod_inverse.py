1f763207241ce6f65d77d7f293b5ae53194079de
Anwesh Mohanty <anwesh063@gmail.com>
2016-06-13 23:39:44 +0530

    Create mod_inverse.py

---
+++ mod_inverse.py
@@ -0,0 +1,14 @@
+# used to convert 1/(A+B+... some expression) to the numerator mod modulo
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
